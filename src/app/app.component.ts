a7842afad7fa2a75771da996150db01451c96680
IvanMonichev <monichevid@baltech.ru>
2023-11-07 16:01:56 +0300

    feat: add form builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [],
 })
 export class AppComponent {
-  title = 'mediumclone-angular';
+  foo(): string {
+    return ''
+  }
+
+  bar(): void {
+    const bar = this.foo()
+  }
 }
